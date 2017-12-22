cc8a5df82465eba2855b944fa116879fc68128c8
Till Carlos <till@tillcarlos.com>
2017-12-22 17:45:01 +0700

    we have sentences separated from words now

--- csv_to_html/csv_to_html.rb
+++ csv_to_html/csv_to_html.rb
@@ -12,22 +12,36 @@ layout: lesson
 ---
 HTML
   row.each do |r|
+    col_file	            = r[0]
+    col_order             = r[1]
+    col_word_en          = r[2]
+    col_word_vn          = r[3]
+    col_text_en          = r[4]
+    col_text_vn          = r[5]
+    col_template          = r[6]
+    col_Level          = r[7]
+    col_Area          = r[8]
+    col_ImageSourceURL = r[9]
+    col_mediafilename_en = r[10]													
+    
     b = binding
     b.local_variable_set('lesson', filename)
-    b.local_variable_set('text_en', r[3])
-    b.local_variable_set('text_vn', r[4])
+    b.local_variable_set('word_en', col_word_en)
+    b.local_variable_set('word_vn', col_word_vn)
     
-    b.local_variable_set('media_name', r[9])
-    b.local_variable_set('has_audio', r[2])
-    b.local_variable_set('has_image', r[8])
+    b.local_variable_set('text_en', col_text_en)
+    b.local_variable_set('text_vn', col_text_vn)
     
-    template = File.read(File.dirname(__FILE__)+'/templates/' + r[5] + '.erb')
+    b.local_variable_set('media_name', col_mediafilename_en)
+    b.local_variable_set('has_image', col_ImageSourceURL)
+    
+    template = File.read(File.dirname(__FILE__)+'/templates/' + col_template + '.erb')
     file.write ERB.new(template).result(b)
     
-    image_output_file = File.dirname(__FILE__)+"/../not-in-git/images_dl/#{r[9]}.jpg"
-    unless r[8].nil? or r[9].nil? or File.exists?(image_output_file)
+    image_output_file = File.dirname(__FILE__)+"/../not-in-git/images_dl/#{col_mediafilename_en}.jpg"
+    unless col_ImageSourceURL.nil? or col_mediafilename_en.nil? or File.exists?(image_output_file)
       #File.write("file_name", Net::HTTP.get(URI.parse(r[8])))
-      `wget  -O #{image_output_file}  #{r[8]}`
+      `wget  -O #{image_output_file}  #{col_ImageSourceURL}`
     end
   end
   file.write <<HTML
