8db813333373ebc43d6c8090186a14b7773a0e90
Till Carlos <till@tillcarlos.com>
2018-02-26 16:01:15 +0700

    content update via deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ cp -r /Users/till/Dropbox/EnglishLetics/_lessons .
 mv _config.yml _config_bkp.yml
 mv _config_trial.yml _config.yml
 jekyll b 
-cp _site/api_* .
+cp -r _site/api_* .
 git add .
 git commit -a -m "content update via deploy.sh"
 git push
