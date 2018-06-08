962b64e3de933e18f51ca187cd973a918e04bd49
Till Carlos <till@tillcarlos.com>
2018-06-08 19:47:56 +0300

    content update via deploy.sh

---
+++ deploy_quyn.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+unlink _lessons || rm -rf _lessons
+cp -r /Users/till/Dropbox/EnglishLetics/_lessons .
+
+mv _config.yml _config_bkp.yml
+
+bundle exec jekyll b
+
+git add . 
+git commit -a -m "content update via deploy.sh"
+git push premium
+
+
+rm -rf _lessons
+ln -s /Users/till/Dropbox/EnglishLetics/_lessons .
+
