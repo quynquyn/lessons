384a9a08618408aa2d45fbb7a0240c29ad6341c7
Till Carlos <till@tillcarlos.com>
2018-06-08 20:00:41 +0300

    content update via deploy.sh

--- deploy_quyn.sh
+++ deploy_quyn.sh
@@ -3,13 +3,13 @@
 unlink _lessons || rm -rf _lessons
 cp -r /Users/till/Dropbox/EnglishLetics/_lessons .
 
-mv _config.yml _config_bkp.yml
+mv _config.yml
 
 bundle exec jekyll b
 
 git add . 
 git commit -a -m "content update via deploy.sh"
-git push quyn
+git push quynquyn
 
 
 rm -rf _lessons
